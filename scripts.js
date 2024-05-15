1b1b3ac226432f01d3c3d08a3a4c18481c7a8410
Maximus <aman.10shaikh@gmail.com>
2024-05-15 10:28:29 +0530

    More changes ^-^

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.hover').forEach(link => {
-  link.addEventListener('mouseenter', () => {
-    link.classList.add('hover-grow');
-  });
-  link.addEventListener('mouseleave', () => {
-    link.classList.remove('hover-grow');
-  });
-});
